5253481d393e287b00982e18ca612b2a73487dff
SatrioAkbar12 <7.1.satrioakbar@gmail.com>
2025-12-27 14:36:00 +0700

    add roles

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,9 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            
+            $table->string('avatar_url')->nullable();
+            $table->foreignId('role_id')->nullable()->constrained()->onDelete('SET NULL')->onUpdate('CASCADE');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
